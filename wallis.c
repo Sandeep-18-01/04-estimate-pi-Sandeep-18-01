5141e4c507a1572e1bc96d56256f7055346f1749
Sandeep-18-01 <85050075+Sandeep-18-01@users.noreply.github.com>
2021-06-03 23:44:50 +0530

    Update wallis.c

--- wallis.c
+++ wallis.c
@@ -1,3 +1,4 @@
+
 #include <assert.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -22,5 +23,17 @@ int main(void) {
       abort();
     }
   }
+}
+float wallis_pi(int i)
+{
+  float k=1;
+  float pi;
+  for(int n=1;n<=i,n++;)
+  {
+    float m=(float)n;
+    k=k*(4*m*m/((4*m*m)-1));
+  }
+  pi=2*k;
+  return pi;
 }
 
